cbbe5c64ff79ead2e0d6fbbfd45368da01f93108
Joshua Braga <jbraga2@huskers.unl.edu>
2022-01-28 15:50:44 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Joshua Braga and Isaiah Hazzard
+ *         
+ * Date: 2022/01/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Joshua Braga and Isaiah Hazzard\n");
+
+  return 0;
+}
